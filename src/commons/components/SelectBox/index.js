b58e1deac3f2ced48d0ecc691602822940dd0848
Tran Minh Hung <hungtranpro2@enjoyworks.co.kr>
2021-03-02 07:01:34 +0000

    Feature/select box

---
+++ src/commons/components/SelectBox/index.js
@@ -0,0 +1,44 @@
+// @flow
+import React, { memo, useState } from 'react';
+
+type Props = {
+  options?: Array,
+  label?: String,
+};
+
+const SelectBox = ({ options, label }: Props) => {
+  const [defaultvalue, setDefaultValue] = useState(options[0]);
+
+  const handleSelectChange = (e) => {
+    setDefaultValue(e.target.value);
+  };
+  return (
+    <div className="form-group">
+      <label htmlFor={label}>{label}</label>
+      <select
+        className="form-control"
+        onChange={(e) => handleSelectChange(e)}
+        value={defaultvalue}
+      >
+        {options.length ? (
+          options.map((option) => {
+            return (
+              <option value={option} key={option}>
+                {option}
+              </option>
+            );
+          })
+        ) : (
+          <option value="">No data</option>
+        )}
+      </select>
+    </div>
+  );
+};
+
+SelectBox.defaultProps = {
+  options: [],
+  label: '',
+};
+
+export default memo<Props>(SelectBox);
