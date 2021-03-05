aa27d88dcd0694971d3d8d430ca631ac16ebf939
HUNG TRAN <hungtranpro2@enjoyworks.co.kr>
2021-03-05 11:32:11 +0700

    up code label

--- src/constants/image.js
+++
@@ -1,9 +0,0 @@
-const IMG = {
-  ICON_POWER: require('assets/images/icon_label/icon-power@3x.png').default,
-  ICON_TEMPERATURE: require('assets/images/icon_label/icon-temperature@3x.png')
-    .default,
-  ICON_INSOLATION: require('assets/images/icon_label/icon-insolation@3x.png')
-    .default,
-};
-
-export default IMG;
