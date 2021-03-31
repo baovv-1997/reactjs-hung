aefbe1242a73af5c64d8269c18262454a11b1614
phanhoquan <phanhoquan@enjoyworks.co.kr>
2021-03-31 18:20:51 +0700

    up code export img chart

--- src/modules/testSolarMonitoringStatus/components/statusDevelop/ItemContentTab.js
+++ src/modules/testSolarMonitoringStatus/components/statusDevelop/ItemContentTab.js
@@ -4,10 +4,10 @@ import Table from 'commons/components/Table';
 import LengthChart from 'commons/components/LengthChart';
 import TitleSubHeader from 'commons/components/TitleHeader/titleSub';
 import { headTestMockupStatus } from '../constant';
-// import LineSeriesChart from 'commons/components/LineChart';
 import BoxGroup from './BoxGroup';
 import GroupCompareChart from './GroupCompareChart';
 import GroupActionDownload from '../GroupActionDownload';
+import { LineChart } from 'commons/components/LineChart/lineChart';
 
 type Props = {
   listMockupDataCompany: any,
@@ -31,21 +31,20 @@ const ItemContentTab = ({
   const dataLengthChart = [
     {
       id: 1,
-      name: '발전량 kWh',
+      name: '발전량(kWh)',
       color: '#8567b4',
     },
     {
       id: 2,
-      name: '일사량 ℃',
+      name: '일사량(W/㎡)',
       color: '#c05e13',
     },
     {
       id: 3,
-      name: '성능비 kWh/㎡·10초',
+      name: '성능비(%)',
       color: '#fe8224',
     },
   ];
-
   return (
     <div className="content-wrap-tab">
       <BoxGroup
@@ -65,7 +64,20 @@ const ItemContentTab = ({
           </div>
         </div>
         <div className="group-char-right">
-          {/* <LineSeriesChart width={1200} height={400} dataChart={dataChart} /> */}
+          <LineChart
+            dataChart={dataChart}
+            unitLine1="kWh"
+            unitLine2="W/㎡"
+            unitLine3="%"
+            optionLine={{
+              line1: paramsSearch?.power,
+              line2: paramsSearch?.insolation,
+              line3: paramsSearch?.performance,
+            }}
+            showPoint3
+            showPoint1
+            showPoint2
+          />
         </div>
       </div>
       <TitleSubHeader title="실시간 계측 현황" />
